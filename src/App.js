738137512e5a974a12619f96cc32289d6a167f42
Gus <gustavo.canales@me.com>
2021-09-29 11:15:35 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.scss";
+import Admin from "./pages/Admin";
+import SignIn from "./pages/Admin/SignIn"
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
+
+function App() {
+  return (
+    <div>
+      <h1>Estamos en App</h1>
+      <Admin/>
+      <SignIn/>
+      <Home/>
+      <Contact/>
+    </div>
+  );
+}
+
+
+
+export default App;
