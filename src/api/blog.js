c5355226e3f653154722f9522fd613cd288abd4f
Gus <gustavo.canales@me.com>
2022-02-07 00:54:21 +0100

    correct headers

--- src/api/blog.js
+++ src/api/blog.js
@@ -4,9 +4,7 @@ import axios from "axios";
 export function getBlogsApi(limit,page){
     const url = `https://netzwerk.mx/get-posts?page=${page}&limit=${limit}`;
     return axios
-      .get(url,{
-        headers: { Accept: "application/json", Origin: "http://104.248.125.58" },
-      })
+      .get(url)
       .then((response) => {
         return response.data;
       })
